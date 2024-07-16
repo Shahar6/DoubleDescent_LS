588f66eafae70d504c4f8ff3c993132dd087dce2
Shahar6 <shaharb4r@gmail.com>
2024-07-16 20:46:07 +0300

    added regularization

--- program.py
+++ program.py
@@ -16,7 +16,7 @@ Sigma = DCT @ delta @ DCT.T
 sigma_eps = 0.25
 beta_tide = np.array([0.2178] * 20 + [0.0218] * 108)
 beta = DCT @ beta_tide
-
+gamma = [0.001, 0.1, 1]
 
 def generate_x(Sigma=Sigma, num_samples=1):
     L = np.linalg.cholesky(Sigma)
@@ -56,6 +56,9 @@ for i in range(test_size):
     test_out[i, :] = y
 # create U
 errors = []
+errors_ridge_small = []
+errors_ridge_medium = []
+errors_ridge_big = []
 params = [i for i in range(1, d + 1)]
 for p in range(1, d + 1):
     U = np.zeros((d, p))
@@ -64,28 +67,49 @@ for p in range(1, d + 1):
         U[:, i] = column
     tetha = train_matrix @ U
     hasInv = np.linalg.det(tetha.T @ tetha) != 0
+    eye_p = np.identity(p)
+    tetha_norm = tetha.T @ tetha
+    tetha_sample_out = tetha.T @ sample_out
+    matrix_save = U.T @ test_matrix.T
     if hasInv:
-        alpha = np.linalg.inv(tetha.T @ tetha) @ tetha.T @ sample_out
+        alpha = np.linalg.inv(tetha_norm) @ tetha_sample_out
     else:
-        alpha = np.linalg.pinv(tetha.T @ tetha) @ tetha.T @ sample_out
-    test_err = (np.linalg.norm((alpha.T @ U.T @ test_matrix.T) - test_out.T) ** 2) / test_size
+        alpha = np.linalg.pinv(tetha_norm) @ tetha_sample_out
+    alpha_ridge_small = np.linalg.inv(tetha_norm + eye_p * gamma[0]) @ tetha_sample_out
+    alpha_ridge_medium = np.linalg.inv(tetha_norm + eye_p * gamma[1]) @ tetha_sample_out
+    alpha_ridge_big = np.linalg.inv(tetha_norm + eye_p * gamma[2]) @ tetha_sample_out
+    test_err = (np.linalg.norm((alpha.T @ matrix_save) - test_out.T) ** 2) / test_size
+    small_err = (np.linalg.norm((alpha_ridge_small.T @ matrix_save) - test_out.T) ** 2) / test_size
+    medium_err = (np.linalg.norm((alpha_ridge_medium.T @ matrix_save) - test_out.T) ** 2) / test_size
+    big_err = (np.linalg.norm((alpha_ridge_big.T @ matrix_save) - test_out.T) ** 2) / test_size
     errors.append(test_err)
+    errors_ridge_small.append(small_err)
+    errors_ridge_medium.append(medium_err)
+    errors_ridge_big.append(big_err)
 
-plt.plot(params, errors, color='b')
-plt.ylim(0, 2.5)
+plt.plot(params, errors, color='b', label='least squares')
+plt.plot(params, errors_ridge_small, color='r', label='gamma=0.001')
+plt.plot(params, errors_ridge_medium, color='g', label='gamma=0.1')
+plt.plot(params, errors_ridge_big, color='m', label='gamma=1')
+plt.ylim(0, 5)
 plt.title('Errors dependent on Parameters in Least Squares')
 plt.xlabel('Parameters')
 plt.ylabel('Error')
+plt.legend()
 plt.show()
 
 K = 500  # times to repeat the experiment
 # create vector to store the errors
 avg_errors = np.zeros(d)
-bias = np.zeros(d)
-variance = np.zeros(d)
+avg_errors_small = np.zeros(d)
+avg_errors_medium = np.zeros(d)
+avg_errors_big = np.zeros(d)
+#bias = np.zeros(d)
+#variance = np.zeros(d)
 Fs = np.zeros((K, test_size))
 
 for p in range(1, d + 1):
+    print(f'iter:{p}')
     Fx_avg = np.zeros((1, test_size))
     U = np.zeros((d, p))
     for i in range(p):
@@ -102,24 +126,41 @@ for p in range(1, d + 1):
             sample_out[i, :] = y
         tetha = train_matrix @ U
         hasInv = np.linalg.det(tetha.T @ tetha) != 0
+        eye_p = np.identity(p)
+        tetha_norm = tetha.T @ tetha
+        tetha_sample_out = tetha.T @ sample_out
+        matrix_save = U.T @ test_matrix.T
         if hasInv:
-            alpha = np.linalg.inv(tetha.T @ tetha) @ tetha.T @ sample_out
+            alpha = np.linalg.inv(tetha_norm) @ tetha_sample_out
         else:
-            alpha = np.linalg.pinv(tetha.T @ tetha) @ tetha.T @ sample_out
-        curr_f = (alpha.T @ U.T @ test_matrix.T)
-        test_err = (np.linalg.norm(curr_f - test_out.T) ** 2) / test_size
+            alpha = np.linalg.pinv(tetha_norm) @ tetha_sample_out
+        #curr_f = (alpha.T @ U.T @ test_matrix.T)
+        #test_err = (np.linalg.norm(curr_f - test_out.T) ** 2) / test_size
+        alpha_ridge_small = np.linalg.inv(tetha_norm + eye_p * gamma[0]) @ tetha_sample_out
+        alpha_ridge_medium = np.linalg.inv(tetha_norm + eye_p * gamma[1]) @ tetha_sample_out
+        alpha_ridge_big = np.linalg.inv(tetha_norm + eye_p * gamma[2]) @ tetha_sample_out
+        test_err = (np.linalg.norm((alpha.T @ matrix_save) - test_out.T) ** 2) / test_size
+        small_err = (np.linalg.norm((alpha_ridge_small.T @ matrix_save) - test_out.T) ** 2) / test_size
+        medium_err = (np.linalg.norm((alpha_ridge_medium.T @ matrix_save) - test_out.T) ** 2) / test_size
+        big_err = (np.linalg.norm((alpha_ridge_big.T @ matrix_save) - test_out.T) ** 2) / test_size
         avg_errors[p - 1] += (test_err / K)
-        Fx_avg += (curr_f / K)
-        Fs[j, :] = curr_f
-    variance[p - 1] = np.sum(np.linalg.norm(Fs - Fx_avg, axis=1) ** 2) / (K * test_size)
-    Fx_avg = np.linalg.norm((Fx_avg - (test_matrix @ beta))) ** 2
-    bias[p - 1] = Fx_avg / test_size
+        avg_errors_small[p - 1] += (small_err / K)
+        avg_errors_medium[p - 1] += (medium_err / K)
+        avg_errors_big[p - 1] += (big_err / K)
+        #Fx_avg += (curr_f / K)
+        #Fs[j, :] = curr_f
+    #variance[p - 1] = np.sum(np.linalg.norm(Fs - Fx_avg, axis=1) ** 2) / (K * test_size)
+    #Fx_avg = np.linalg.norm((Fx_avg - (test_matrix @ beta))) ** 2
+    #bias[p - 1] = Fx_avg / test_size
 
 plt.plot(params, avg_errors, color='b', label='Average Test Error')
-plt.plot(params, bias, color='r', ls='--', dashes=(5, 5), label='Bias Squared')
-plt.plot(params, variance, color='g', ls='--', dashes=(5, 5), label='Variance')
-plt.title('Error, bias and variance dependent on Parameters in Least Squares')
+plt.plot(params, avg_errors_small, color='r', label='gamma=0.001')
+plt.plot(params, avg_errors_medium, color='g', label='gamma=0.1')
+plt.plot(params, avg_errors_big, color='m', label='gamma=1')
+#plt.plot(params, bias, color='c', ls='--', dashes=(5, 5), label='Bias Squared')
+#plt.plot(params, variance, color='y', ls='--', dashes=(5, 5), label='Variance')
+plt.title('Error, ridge regression error dependent on Parameters in Least Squares')
 plt.xlabel('Parameters')
-plt.ylim(0, 2.5)
+plt.ylim(0, 5)
 plt.legend()
 plt.show()
